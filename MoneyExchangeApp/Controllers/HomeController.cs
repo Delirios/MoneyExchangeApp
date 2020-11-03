43a89f39bfea29d707941a556af567ee182afd08
Delirios <lushchevskiyd@gmail.com>
2020-11-03 19:36:35 +0200

    Created project infrastructure

---
+++ MoneyExchangeApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MoneyExchangeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+    }
+}
